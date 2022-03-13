fe9aec0dee8cb9ab771f50cddd5373ffe23d7f12
CRYZUEZIN <62780115+CRYZUEZIN@users.noreply.github.com>
2022-03-13 19:42:50 -0300

    Add files via upload

--- tools/unpack_super.sh
+++ tools/unpack_super.sh
@@ -1,1 +1,1 @@
-cd .. && ./lpunpack super.img super_raw.img
+cd .. && ./lpunpack super.img super_raw.img; rm system_b.img system_ext_b.img product_b.img vendor_b.img; mv system_a.img system_a_old.img; mv product_a product_a_old.img; mv vendor_a.img vendor_a_old.img
