4bbcc8b6db2bceffc6ee35c0be03d9faa33e9e6a
CRYZUEZIN <62780115+CRYZUEZIN@users.noreply.github.com>
2022-03-20 21:40:15 -0300

    Update unpack_super.sh

--- tools/unpack_super.sh
+++ tools/unpack_super.sh
@@ -1,1 +1,1 @@
-cd .. && ./lpunpack super_raw.img; rm system_b.img system_ext_b.img product_b.img vendor_b.img; mv system_a.img system_a_old.img; mv product_a.img product_a_old.img; mv vendor_a.img vendor_a_old.img
+cd .. && ./lpunpack super_raw.img; rm system_b.img system_ext_b.img product_b.img vendor_b.img; mv system_a.img system_a_old.img; mv system_ext_a.img system_ext_a_old.img; mv product_a.img product_a_old.img; mv vendor_a.img vendor_a_old.img
